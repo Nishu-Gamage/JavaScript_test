7c10c6b98d75ee6cb50d9ffbbe8cb06758a2de94
Nishu <67893576+NishuGamage@users.noreply.github.com>
2022-06-09 10:19:26 +0900

    Import external javaScript file

---
+++ main.js
@@ -0,0 +1,30 @@
+/*---------------------------------------------------------------------
+   *  Chrome has a V8 engine option to run these javaScript codes.
+   *  Variables are case sensitive.
+   *  <nonscript> code use to diaplay warning message to user,
+          if SEO does not have javascript.
+   *  Data types are,
+          Numbers , String and Boolean.
+   * JavaScript also defines two data types,
+          null and undifined.
+-----------------------------------------------------------------------*/
+
+
+function syaHello(){
+     document.write ("This is main JS");
+     alert("Hellow! This is main JS");
+}
+
+ // Variable
+ var1 = 10
+ var2 = 20
+ // best practices ( ; )
+ var3 = 30; var4 = 40;
+
+
+// varibale
+var x = null;
+var y = 'Display data from  js variable'
+
+// print variable
+document.write(x);
