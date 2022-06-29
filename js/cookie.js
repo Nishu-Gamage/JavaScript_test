94d84a64fb17691aafab253fb56bff1ce5e7171c
Nishu <67893576+NishuGamage@users.noreply.github.com>
2022-06-29 14:37:46 +0900

    cookie with xampp testing

--- js/cookie.js
+++ js/cookie.js
@@ -1,3 +1,66 @@
+
+// <!------------------------- nishu test 2022/6/6 ------------------------->
+     function SetCookie(){
+          document.cookie = 'name= Nishu';
+          cookieDisplay.textContent = "Display set cookie hear : " + document.cookie ;   
+          console.log(document.cookie) 
+          
+          var x = document. cookie; window. alert(x); 
+     }
+     
+     function GetCookie(){
+          var allcookies = document.cookie;
+          console.log(allcookies);
+          console.log ("All Cookies : " + allcookies );
+
+          // Get all the cookies pairs in an array
+          cookiearray = allcookies.split(';');
+
+          // Now take key value pair out of this array
+          for(var i=0; i<cookiearray.length; i++){
+               name = cookiearray[i].split('=')[0];
+               value = cookiearray[i].split('=')[1];
+               console.log ("Key is : " + name );
+               console.log ("Value is : " + value);
+          }
+          cookieDisplay.textContent = "Display cookie : " + allcookies ;
+     }
+     
+// ---------------------------------------------------------------------------------
+     // Class -  study 
+// ---------------------------------------------------------------------------------
+
+document.cookie ="name=Nishu"
+document.cookie = "username=John Doe";
+// let x;
+
+console.log(document.cookie);
+
+function setCookie() {
+     document.cookie = "username=John Doe"; 
+     // document.cookie = "username=John Doe; expires=Thu, 18 Dec 2013 12:00:00 UTC";
+     // document.cookie = "username=John Doe; expires=Thu, 18 Dec 2013 12:00:00 UTC; path=/"; 
+     let x = document.cookie;
+     console.log(document.cookie);
+
+}
+
+function getCookie(name) {
+     var nameEQ = name + "=";
+     var ca = document.cookie.split(';');
+     for(var i=0;i < ca.length;i++) {
+          var c = ca[i];
+          while (c.charAt(0)==' ') c = c.substring(1,c.length);
+          if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+     }
+     return null;
+}
+
+function eraseCookie(name) {   
+     document.cookie = name +'=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+}
+
+
 //------------------------- test - 1 -------------------------
 function WriteCookie(){
      if( document.myform.customer.value == "" ){
